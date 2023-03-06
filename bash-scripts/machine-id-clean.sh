6922cac036105caf9e04c628bb18cb32b58a5e01
kosmolito <said.karker@gmail.com>
2023-03-06 10:38:39 +0100

    Intended to run PRE template convertion The script cleans the machine and removes machine-id information. Important to run this script before making a template of the VM, in order to avoid having network problems, where multiple machines will have same ip address

---
+++ bash-scripts/machine-id-clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo cloud-init clean
+sudo rm -rf /var/lib/cloud/instances
+sudo truncate -s 0 /etc/machine-id
+sudo rm /var/lib/dbus/machine-id
+sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
+echo "Machine is clean. Powering off to make it a Template!"
+sudo poweroff
