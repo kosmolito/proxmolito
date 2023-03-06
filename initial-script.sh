aaee87b7b88fa8e88422f61d18f477ce48aea9b5
kosmolito <said.karker@gmail.com>
2023-03-06 10:22:52 +0100

    initial script to be run before the mail script It update / install packaes that are required / good to have and make a update of the system overall

---
+++ initial-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script is intended to be run on a fresh install of Proxmox VE
+apt update
+apt install -y curl wget nano git vim curl
+apt -y full-upgrade
+
+########## Install PowerShell ##########
+# Install system components
+apt update  && apt install -y curl gnupg apt-transport-https
+
+# Import the public repository GPG keys
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+# For NON Enterprise List, delete or rename the Enterprise source list file in "/etc/apt/sources.list.d/", otherwise it will show "non authorized repo"
+mv /etc/apt/sources.list/pve-enterprise.list /etc/apt/sources.list/pve-enterprise.list.bak
+
+# Register the Microsoft Product feed
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
+
+# Install PowerShell
+apt update && apt install -y powershell
+
+# Restart the shell so that PATH changes take effect
+[ -f /var/run/reboot-required ] && reboot -f
