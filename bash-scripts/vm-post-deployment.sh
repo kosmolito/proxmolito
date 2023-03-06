a153a6017ddad54f313dcf2c026e7bfdc397e9c5
kosmolito <said.karker@gmail.com>
2023-03-06 10:34:42 +0100

    Script to be run after VM deployment. It updates the system and install some utility applications

---
+++ bash-scripts/vm-post-deployment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script is intended to be run on a fresh install ubuntu vm
+sudo apt update && sudo apt upgrade
+
+# Install system components
+sudo apt install -y qemu-guest-agent nano curl wget nano git gnupg iputils-ping
+
+# ########## Install PowerShell ##########
+
+# Update the list of packages
+sudo apt-get update
+# Install pre-requisite packages.
+sudo apt-get install -y wget apt-transport-https software-properties-common
+# Download the Microsoft repository GPG keys
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+# Update the list of packages after we added packages.microsoft.com
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
+
+# Restart the shell so that PATH changes take effect
+nohup sudo reboot &>/dev/null & exit
