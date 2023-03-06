298352a72953d9fa160825705bdba0ce16dd6274
kosmolito <said.karker@gmail.com>
2023-03-06 10:45:18 +0100

    Show the menu in a loop and switch statements

--- proxmox-tools.ps1
+++ proxmox-tools.ps1
@@ -22,3 +22,87 @@ function Show-Menu {
     Write-Host "c:" "Clear the screen" -ForegroundColor Green
     Write-Host "Q: To quit" -ForegroundColor Green
 }
+
+
+do {
+    Show-Menu -Title "Proxmox VM Utilities"
+    $Selection = Read-Host "Please make a selection"
+    # Get the current location
+    $Location = Get-Location
+    switch ($Selection) {
+        "c" { Clear-Host }
+        "1" 
+        {
+            & $Location\vm-deployments\ubuntu-2204-cloudinit.ps1
+        }
+
+        "2" 
+        {
+            Write-Host "Install Required packaes and update the VM" -ForegroundColor Green | Out-Host
+            if ($VMIP -like "") {
+                $VMIP = Read-Host "Enter the IP address for the VM"
+            } else {
+                Write-Host "Using the IP address from the previous step: $VMIP" -ForegroundColor Yellow
+                Pause
+            }
+
+            $SourceFolder = "$($Location)/bash-scripts"
+            $SourceFileName = "vm-post-deployment.sh"
+            $SourceFile = "$SourceFolder/$SourceFileName"
+            $DestinationFile = "/tmp/$SourceFileName"
+
+            scp -o "StrictHostKeyChecking=no" $SourceFile $($VMIP):$($DestinationFile)
+            ssh $VMIP $DestinationFile
+        }
+
+
+        "3" 
+        {
+            Write-Host "Installing K8s and all the dependencies" -ForegroundColor Green
+            if ($VMIP -like "") {
+                $VMIP = Read-Host "Enter the IP address for the VM"
+            } else {
+                Write-Host "Using the IP address from the previous step: $VMIP" -ForegroundColor Yellow
+                Pause
+            }
+            
+
+            $SourceFolder = "$($Location)/bash-scripts"
+            $SourceFileName = "k8s-deployment.sh"
+            $SourceFile = "$SourceFolder/$SourceFileName"
+            $DestinationFile = "/tmp/$SourceFileName"
+
+            scp -o "StrictHostKeyChecking=no" $SourceFile $($VMIP):$($DestinationFile)
+            ssh $VMIP $DestinationFile
+
+        }
+
+
+        "4"
+        {
+            ################## Clean the machine to make a Template ###################
+            Write-Host "Cleaning the machine to make a Template" -ForegroundColor Green
+            if ($VMIP -like "") {
+                $VMIP = Read-Host "Enter the IP address for the VM"
+            } else {
+                Write-Host "Using the IP address from the previous step: $VMIP" -ForegroundColor Yellow
+                Pause
+            }
+
+            $SourceFolder = "$($Location)/bash-scripts"
+            $SourceFileName = "machine-id-clean.sh"
+            $SourceFile = "$SourceFolder/$SourceFileName"
+            $DestinationFile = "/tmp/$SourceFileName"
+
+            scp -o "StrictHostKeyChecking=no" $SourceFile $($VMIP):$($DestinationFile)
+            ssh $VMIP $DestinationFile
+
+        }
+        
+        Default {}
+    }
+
+
+} until (
+    $Selection -eq "q"
+)
