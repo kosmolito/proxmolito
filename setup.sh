228b0e9daabf019e60e74192dedbcba6154ba4e3
kosmolito <said.karker@gmail.com>
2023-03-06 11:52:36 +0100

    Code cleanup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 # Make the scripts in bash-scripts folder executable
 chmod +x bash-scripts/*.sh
 
+# For NON Enterprise List, rename the Enterprise source list file in "/etc/apt/sources.list.d/", otherwise it will show "non authorized repo"
+[ -f /etc/apt/sources.list/pve-enterprise.list ] && mv /etc/apt/sources.list/pve-enterprise.list /etc/apt/sources.list/pve-enterprise.list.bak -f
+
 apt update
 apt install -y curl wget nano git vim curl
 apt -y full-upgrade
@@ -14,9 +17,6 @@ apt update  && apt install -y curl gnupg apt-transport-https
 
 # Import the public repository GPG keys
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-
-# For NON Enterprise List, delete or rename the Enterprise source list file in "/etc/apt/sources.list.d/", otherwise it will show "non authorized repo"
-mv /etc/apt/sources.list/pve-enterprise.list /etc/apt/sources.list/pve-enterprise.list.bak
 
 # Register the Microsoft Product feed
 sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list'
