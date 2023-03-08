dc6e97254daf4acd5ed771b78e9c52f78da4cfc6
kosmolito <said.karker@gmail.com>
2023-03-08 23:37:44 +0100

    Message of the steps showing on the screen

--- bash-scripts/machine-id-clean.sh
+++ bash-scripts/machine-id-clean.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+echo "Cleaning machine-id"
 sudo cloud-init clean
+
+echo "Removing cloud-init files"
 sudo rm -rf /var/lib/cloud/instances
+
+echo "Removing machine-id in /etc and /var/lib/dbus"
 sudo truncate -s 0 /etc/machine-id
 sudo rm /var/lib/dbus/machine-id
+
+echo "Creating symlink for machine-id"
 sudo ln -s /etc/machine-id /var/lib/dbus/machine-id
+
 echo "Machine is clean. Powering off to make it a Template!"
+
+echo "You can now create a new VM from this Template"
 sudo poweroff
